da566422a04b8d62d9b994975ac983a9d03c0a65
Gabriel Lima <devlimagabriel@gmail.com>
2021-05-25 21:01:44 -0300

    build: add configs for typescript and babel next

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,9 @@ module.exports = {
   presets: [
     ["@babel/preset-env", { targets: { node: "current" } }],
     ["@babel/preset-react"],
+    ["@babel/preset-flow"],
+    ["@babel/preset-typescript"],
+    ["next/babel"],
   ],
+  plugins: ["babel-plugin-styled-components"],
 };
